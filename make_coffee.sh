2d62d8601eb98ca12702a82996425b6212875bbb
Johannes Graf <graf@synyx.de>
2017-11-17 22:42:23 +0100

    it works :-)

--- make_coffee.sh
+++ make_coffee.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "kaffee mahlen"
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:07'
 sleep 3s
@@ -11,21 +10,13 @@ echo "Schieber oeffnen / rumfahren"
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:11'
 sleep 5s
 
-echo "drainage valve on"
-mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:1D'
-sleep 5s
-
-echo "brewgroup into brewing position"
-mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:13'
-sleep 5s
-
 echo "wasser aufheizen"
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:03'
 sleep 2s
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:04'
 
 echo "wasser ansaugen"
-  mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:01'
+mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:01'
 sleep 1s
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:02'
 sleep 1s
@@ -35,6 +26,18 @@ mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f70
 sleep 20s
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:02'
 sleep 3s
+
+echo "drainage valve on"
+mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:1D'
+sleep 1s
+
+echo "squeeze it"
+mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:12'
+sleep 3s
+
+echo "drainage valve off"
+mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:1E'
+sleep 1s
 
 echo "leeren"
 mosquitto_pub -h iot-central.synyx.coffee -p 1883 -t 'coffee/command/a020a600f704' -m 'FN:0E'
